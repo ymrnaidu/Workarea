5daec22c7776e9194895cfc9e2df77474e7c9602
ymrnaidu <48776143+ymrnaidu@users.noreply.github.com>
2021-11-13 10:43:27 -0500

    Add files via upload

--- solution.py
+++ solution.py
@@ -126,16 +126,25 @@ def ping(host, timeout=1):
     # Calculate vars values and return them
     
     #vars = [float(round(packet_min, 2)), float(round(packet_avg, 2)), float(round(packet_max, 2)), float(round(statistics.stdev(stdev_var), 2))]
-    vars = [float(round(packet_min,0)), float(round(packet_avg,2)), float(round(packet_max,0)), float(round(stdev_var,2))]
+    #vars = [float(round(packet_min,0)), float(round(packet_avg,2)), float(round(packet_max,0)), float(round(stdev_var,2))]
     # Send ping requests to a server separated by approximately one second
     for i in range(0,4):
         delay = doOnePing(dest, timeout)
         print(delay)
-        print(vars)
+        #print("dest :" + dest)
         time.sleep(1)  # one second
-    
+    packet_min = min(delay) 
+    #print(packet_min)
+    packet_max = max(delay)
+    #print(packet_max)
+    packet_avg = statistics.mean(delay)
+    #print(packet_avg)
+    stdev_var = statistics.stdev(delay)
+    #print(stdev_var)
+    vars = [float(round(packet_min,0)), float(round(packet_avg,2)), float(round(packet_max,0)), float(round(stdev_var,2))]
+    #print(vars)
     return vars
-    return delay
+    #return delay
     
 if __name__ == '__main__':
     ping("google.co.il")
