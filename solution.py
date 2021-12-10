b374d015b452e3ba4098795faff59a0b4117b3fb
ymrnaidu <48776143+ymrnaidu@users.noreply.github.com>
2021-12-10 12:12:20 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -23,9 +23,9 @@ def welcome_assignment_answers(question):
         answer = "42b76fe51778764973077a5a94056724"
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
         answer = "No"
-    elif question ==  "What layer from the TCP/IP model the protocol DHCP belongs to? ":
+    elif question ==  "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":
         answer = 5
-    elif question ==  "What layer of the TCP/IP model the protocol TCP belongs to? ":
+    elif question ==  "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
         answer = 4
     return(answer)
 # Complete all the questions.
